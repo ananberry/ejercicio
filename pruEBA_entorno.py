7d16b0bbec1678e0c9737ec0b95d28337895e58f
ananberry <ana.qskz@gmail.com>
2026-02-23 18:16:17 -0500

    Add files via upload

---
+++ pruEBA_entorno.py
@@ -0,0 +1,5 @@
+from colorama import Fore, init
+
+init()#
+print(Fore.BLUE + " ¡El entorno virtual esta funcionando pepfectamente!" )
+print(Fore.RED + "Si ves este texto rojo, la libreria externa se instalo bien")
